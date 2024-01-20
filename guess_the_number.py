dc990a2adb5c6ee167b448ed1bbf0dcfe9cf4c75
IvayloAngelov93 <ivailoangelov1993@gmail.com>
2024-01-20 23:14:12 +0200

    littel change in the colors in the print

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,15 +1,21 @@
 from random import randint
+def print_green(skk): print("\033[92m {}\033[00m" .format(skk))
+def print_red(skk): print("\033[91m {}\033[00m" .format(skk))
+def print_yellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def print_light_purple(skk): print("\033[94m {}\033[00m" .format(skk))
+
 computer_number = randint(1, 100)
+
 while True:
     player_input = input('Guess the number between 1 and 100: ')
     if not player_input.isdigit():
-        print('Invalid input. Try again...')
+        print_green('Invalid input. Try again...')
         continue
     player_input = int(player_input)
     if player_input == computer_number:
-        print('You guess it!')
+        print_red('You guess it!')
         break
     elif player_input > computer_number:
-        print('Too high!')
+        print_yellow('Too high!')
     else:
-        print('Too low!')
+        print_light_purple('Too low!')
